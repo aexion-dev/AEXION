ffb41438dc5087e94c0c4fa40f3dbfcd0e1d8962
Alexandher <12073184+aexion-dev@users.noreply.github.com>
2019-12-02 16:44:28 -0600

    vue routing
    fixed vue routing on portfolio page

--- js/vue-router.js
+++ js/vue-router.js
@@ -1,8 +1,8 @@
 
-
-var app = new Vue({
-    el: '#app',
-    data: {
+const Home = {
+    template: '#home',
+    data() {
+      return {
         currentSlide: 0,
         isPreviousSlide: false,
         isFirstLoad: true,
@@ -14,7 +14,8 @@ var app = new Vue({
                 sublineSecondLine: "APP DEVELOPMENT",
                 bgImg: "img/ididwhat_bg.jpg",
                 rectImg: "img/ididwhat_thumb.png",
-                icoImg: "img/ididwhat_icon.png"
+                icoImg: "img/ididwhat_icon.png",
+                routeURL: "/i-did-what"
             },
             {
                 headlineFirstLine: "ELISIAN",
@@ -23,7 +24,8 @@ var app = new Vue({
                 sublineSecondLine: "PACKAGE DESIGN",
                 bgImg: "img/elisian_bg.jpg",
                 rectImg: "img/elisian_thumb.png",
-                icoImg: "img/elisian_icon.png"
+                icoImg: "img/elisian_icon.png",
+                routeURL: "/i-did-what"
             },
             {
                 headlineFirstLine: "AMS",
@@ -32,7 +34,8 @@ var app = new Vue({
                 sublineSecondLine: "WEB DESIGN",
                 bgImg: "img/ams_bg.jpg",
                 rectImg: "img/ams_thumb.png",
-                icoImg: "img/ams_icon.png"
+                icoImg: "img/ams_icon.png",
+                routeURL: "/i-did-what"
             },
             {
                 headlineFirstLine: "COYOTE",
@@ -41,7 +44,8 @@ var app = new Vue({
                 sublineSecondLine: "BRAND IDENTITY",
                 bgImg: "img/cbc_bg.jpg",
                 rectImg: "img/cbc_thumb.png",
-                icoImg: "img/cbc_icon.png"
+                icoImg: "img/cbc_icon.png",
+                routeURL: "/i-did-what"
             },
             {
                 headlineFirstLine: "",
@@ -50,30 +54,32 @@ var app = new Vue({
                 sublineSecondLine: "WEB DESIGN",
                 bgImg: "img/compass_bg.jpg",
                 rectImg: "img/compass_thumb.png",
-                icoImg: "img/compass_icon.png"
+                icoImg: "img/compass_icon.png",
+                routeURL: "/i-did-what"
             }
         ]
+      }
     },
-  mounted: function () {
-    var productRotatorSlide = document.getElementById("app");
-        var startX = 0;
-        var endX = 0;
+    mounted() {
+      var productRotatorSlide = document.getElementById("app");
+      var startX = 0;
+      var endX = 0;
 
-        productRotatorSlide.addEventListener("touchstart", (event) => startX = event.touches[0].pageX);
+      productRotatorSlide.addEventListener("touchstart", (event) => startX = event.touches[0].pageX);
 
-        productRotatorSlide.addEventListener("touchmove", (event) => endX = event.touches[0].pageX);
+      productRotatorSlide.addEventListener("touchmove", (event) => endX = event.touches[0].pageX);
 
-        productRotatorSlide.addEventListener("touchend", function(event) {
-            var threshold = startX - endX;
+      productRotatorSlide.addEventListener("touchend", function(event) {
+          var threshold = startX - endX;
 
-            if (threshold < 150 && 0 < this.currentSlide) {
-                this.currentSlide--;
-            }
-            if (threshold > -150 && this.currentSlide < this.slides.length - 1) {
-                this.currentSlide++;
-            }
-        }.bind(this));
-  },
+          if (threshold < 150 && 0 < this.currentSlide) {
+              this.currentSlide--;
+          }
+          if (threshold > -150 && this.currentSlide < this.slides.length - 1) {
+              this.currentSlide++;
+          }
+      }.bind(this));
+    },
     methods: {
         updateSlide(index) {
             index < this.currentSlide ? this.isPreviousSlide = true : this.isPreviousSlide = false;
@@ -81,4 +87,42 @@ var app = new Vue({
             this.isFirstLoad = false;
         }
     }
-})
+
+}
+
+const IDidWhat = {
+    template: '#project-template'
+};
+
+const Elisian = {
+    template: '#project-template'
+};
+
+const AMSConstruction = {
+    template: '#project-template'
+};
+
+const CoyoteBoxing = {
+    template: '#project-template'
+};
+
+const Compass = {
+    template: '#project-template'
+};
+
+const routes = [
+    { path: '', component: Home },
+    { path: '/i-did-what', component: IDidWhat },
+    { path: '/elisian', component: Elisian },
+    { path: '/ams-construction', component: AMSConstruction },
+    { path: '/coyote-boxing', component: CoyoteBoxing },
+    { path: '/compass', component: Compass }
+]
+
+const router = new VueRouter({
+    routes
+});
+
+new Vue({
+    router
+}).$mount('#app');
