1c04489b7fe4650e047be25f43cc5c11f1a33010
Aexi0n <Aexi0n@users.noreply.github.com>
2019-12-22 12:43:24 -0600

    Merge branch 'master' of https://github.com/aexion-dev/AEXION

--- js/main.js
+++ js/main.js
@@ -35,17 +35,9 @@ $(document).ready(function() {
         || (navigator.msMaxTouchPoints > 0));
   }
 
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-  // We listen to the resize event
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  /*if (is_touch_device()) {
+    document.getElementById('start-msg').innerHTML='[ TAP HERE TO START ]';
+  }*/
 
   //User Clicks Start Message
   startmsg.click(function(){
