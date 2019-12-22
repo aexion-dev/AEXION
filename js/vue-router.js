82181614c7d868f9475169bdc478c538a41a15b1
Aexi0n <Aexi0n@users.noreply.github.com>
2019-12-22 12:44:26 -0600

    update

--- js/vue-router.js
+++ js/vue-router.js
@@ -63,7 +63,19 @@ const Home = {
         ]
       }
     },
-    mounted() {/*
+    mounted() {
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+      // We listen to the resize event
+      window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      });
+      /*
       var productRotatorSlide = document.getElementById("app");
       var startX = 0;
       var endX = 0;
@@ -106,6 +118,7 @@ const Home = {
 const IDidWhat = {
     template: '#project-template',
     mounted() {
+
       var Scrollbar = window.Scrollbar;
       const scrollbar = Scrollbar.init(document.querySelector('#scrollbar'), {
         damping: 0.1
@@ -113,6 +126,18 @@ const IDidWhat = {
 
       document.querySelector('#project-scroll').addEventListener("click", function(event) {
         scrollbar.scrollIntoView(document.querySelector('#project-start'));
+      });
+
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+      // We listen to the resize event
+      window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
       });
     },
 };
